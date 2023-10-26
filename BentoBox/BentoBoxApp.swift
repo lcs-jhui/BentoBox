6969844bf4f50beeb25ddb32f9cc6ec7e02a4fa1
Justin Hui <jhui@lcs.on.ca>
2023-10-26 15:29:49 -0400

    Initial Commit

---
+++ BentoBox/BentoBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BentoBoxApp.swift
+//  BentoBox
+//
+//  Created by Justin Hui on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BentoBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
